abe815871a287b372efc08729d630bdd015187a2
Numline1 <me@numli.net>
2020-03-16 23:28:34 +0100

    Improved look of the TechnologyListEntry components

--- components/TechnologyListEntry.vue
+++ components/TechnologyListEntry.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex flex-col items-center mx-8 mb-16 w-24">
-    <img :src="technology.icon" :alt="technology.name" />
+  <div class="flex flex-col items-center m-16">
+    <img class="h-24 w-24" :src="technology.icon" :alt="technology.name" />
 
     <p class="pt-4 font-bold text-center">{{ technology.name }}</p>
   </div>
