2c4ba6bdace4a75ffcb6557f9b8ef5401daac08a
Numline1 <me@numli.net>
2020-03-18 19:44:02 +0100

    Reworked the footer section

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
     <p class="p-4 text-center text-gray-700">
-      &copy; Martin Krajcovic | This website on
-      <a
-        class="border-b border-dashed border-gray-900"
-        href="https://github.com/Numline1/numlinet"
-        target="_blank"
-        >GitHub</a
-      >
-      | Built using
-      <a
-        class="border-b border-dashed border-gray-900"
-        href="https://nuxtjs.org/"
-        target="_blank"
-        >NuxtJS</a
-      >
-      | Shoutouts to
-      <a
-        class="border-b border-dashed border-gray-900"
-        href="https://github.com/konpa/devicon/"
-        target="_blank"
-        >Devicon v2</a
-      >
+      &copy; {{ year }} Martin Krajcovic
     </p>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    year: new Date().getFullYear()
+  })
+}
+</script>
