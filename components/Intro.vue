c2d2d27df71787eafae7744263c189de72a312ba
Numline1 <me@numli.net>
2020-03-18 22:09:42 +0100

    Updated intro to break correctly on all resolutions

--- components/Intro.vue
+++ components/Intro.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="mt-32">
     <div class="flex flex-col items-center">
-      <p class="w-2/5 leading-tight px-8 text-6xl font-bold text-center">
-        Senior backend engineer
+      <p class="leading-tight px-8 text-6xl font-bold text-center">
+        Senior backend
+        <br />
+        engineer
       </p>
 
-      <p class="mt-16 w-1/3 text-2xl text-center text-gray-700">
-        Designing and creating stable, scalable, maintainable and tested
-        software
+      <p class="mt-16 text-2xl text-center text-gray-700">
+        Designing and creating stable, scalable, maintainable
+        <br />
+        and tested software
       </p>
 
       <IntroCode></IntroCode>
