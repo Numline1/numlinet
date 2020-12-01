1efbb4355ee3cf42df466a2cd55d7c4a3f00f35b
Numline1 <me@numli.net>
2020-12-01 20:22:31 +0100

    Home page refactor

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,27 @@
 <template>
   <div>
-    <!-- Header -->
-    <Header></Header>
-
-    <!-- Intro -->
     <Intro></Intro>
 
-    <!-- About -->
     <About></About>
 
-    <!-- Career -->
     <TimelineList></TimelineList>
 
-    <!-- Contact -->
     <Contact></Contact>
-
-    <!-- Footer -->
-    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
 import Intro from '@/components/Intro'
 import About from '@/components/About'
 import TimelineList from '@/components/TimelineList'
 import Contact from '@/components/Contact'
-import Footer from '@/components/Footer'
 
 export default {
   components: {
-    Header,
     Intro,
     About,
     TimelineList,
     Contact,
-    Footer
   },
-
-  head() {
-    return {
-      title: 'Numlinet'
-    }
-  }
 }
 </script>
