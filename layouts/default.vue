666a88b03481bde8432811f9420b77ea6e2e5ecd
Numline1 <me@numli.net>
2020-12-02 03:43:45 +0100

    Revert inline Ackee implementation

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,12 +3,6 @@
     <Header></Header>
     <nuxt class="flex-grow mt-8" />
     <Footer></Footer>
-    <script
-      async
-      src="https://ackee.zeus.numli.net/tracker.js"
-      data-ackee-server="https://ackee.zeus.numli.net"
-      data-ackee-domain-id="52aada0a-db2d-439d-aea2-c7434e32b21c"
-    ></script>
   </div>
 </template>
 
