a26d63c1159525daee9779712df696ea782cb030
Numline1 <me@numli.net>
2022-03-14 00:22:34 +0100

    Add LinkedIn link

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,13 @@
 <template>
   <ul class="flex items-center space-x-4">
     <li>
+      <a
+        href="https://www.linkedin.com/in/martin-kraj%C4%8Dovi%C4%8D-1955121a5/"
+        target="_blank"
+        ><img class="h-6" src="/icons/linkedin.svg" alt="Twitter"
+      /></a>
+    </li>
+    <li>
       <a href="https://twitter.com/Numline1" target="_blank"
         ><img class="h-6" src="/icons/twitter.svg" alt="Twitter"
       /></a>
